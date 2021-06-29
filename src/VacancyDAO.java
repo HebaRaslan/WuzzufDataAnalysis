e34b06e4e9fe995674b35226a637ba3725cbd65d
HebaRaslan <76179061+HebaRaslan@users.noreply.github.com>
2021-06-29 23:00:42 +0200

    Update VacancyDAO.java

--- src/VacancyDAO.java
+++ src/VacancyDAO.java
@@ -3,6 +3,23 @@ import java.util.List;
 
 public class VacancyDAO
 {
+        File vacancyFile= new File("file path");
+		List<String>lines = new ArrayList<String>();
+		try {
+			lines=Files.readAllLines(vacancyFile.toPath());
+		}catch(Exception e) {
+			System.out.println("file not found");
+		}
+		for(int i=1;i<lines.size();i++) {
+			String line=lines.get(i);
+			String []fields=line.split(",");
+			 for (int i1 =0;i1<fields.length; i1++) {
+				 fields[i1]=fields[i1].trim();
+				
+			}
+			 Vacancy v = new vacancy(fields[0],fields[1],fields[2],fields[3],field[4],Integer.parseInteger(fields[5]),fields[6],fields for list );
+			 vacancylst.add(v);
+    
     List<Vacancy> vacancylst;
     String path;
     public VacancyDAO(String path) {
